7be5fc861f1b6f6ba15df99a5a3d1162ff603983
BP <26972590+14paxton@users.noreply.github.com>
2025-07-16 12:53:26 -0700

    document for dependencies macos arm

--- build.gradle.kts
+++ build.gradle.kts
@@ -40,15 +40,6 @@ java {
   }
 }
 
-//** building on mac ** //
-// buildscript {
-//    dependencies {
-//        classpath("com.github.docker-java:docker-java-transport-httpclient5:3.4.0") {
-//            because("M1 macs need a later version of JNA")
-//        }
-//    }
-//}
-
 // shadowJar {
 //    archiveBaseName.set('shadow') // Set the base name of the jar
 //    archiveClassifier.set('')
@@ -73,6 +64,12 @@ dependencies {
   annotationProcessor("io.micronaut:micronaut-http-validation")
   annotationProcessor("io.micronaut.validation:micronaut-validation-processor")
   annotationProcessor("io.micronaut.serde:micronaut-serde-processor")
+
+  // *** For ARM MacOS Silicone *** //
+  // - Netty native DNS resolver for MacOS (ARM & Intel)
+  //    runtimeOnly("io.netty:netty-resolver-dns-native-macos:4.1.109.Final")
+  // - Docker Java client (optional: only if using Docker/Testcontainers)
+  //    implementation("com.github.docker-java:docker-java-transport-httpclient5:3.4.0")
 }
 
 micronaut {
